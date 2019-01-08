9f67914c8a45fd2e5426d36b2850916850d2d4bf
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2019-01-08 18:43:58 -0500

    Added SurfacePolygon

---
+++ src/SurfacePolygon.cpp
@@ -0,0 +1,30 @@
+#include "SurfacePolygon.h"
+#include "PolygonUtils.h"
+
+SurfacePolygon::SurfacePolygon(const Polygon p, const Color c): 
+	_poly {p}, _color {c} {
+}
+
+MaybeVector SurfacePolygon::intersection(const Line& ray) const {
+	using ::intersection;
+
+	const Intersection inter {intersection(_poly, ray)};
+	if (const auto solp = std::get_if<Vector>(&inter))
+		return {true, *solp};
+	else
+		return {false, {0, 0, 0}};
+}
+
+Vector SurfacePolygon::normal(const Vector& point, const Vector& light) const {
+	using ::normal;
+
+	const Vector v {light - point};
+	const Vector n {normal(_poly.plane())};
+
+	return n*v > 0 ? n : -n;
+}
+
+Color SurfacePolygon::color(const Vector& point) const {
+	return _color;
+}
+

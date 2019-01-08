4506abb8aa26dd98a241312c23d9e1ff926c4b74
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2019-01-08 18:58:03 -0500

    Fixed missing return type

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -3,10 +3,10 @@
 Ray::Ray(const Vector& dir, const Vector& off): line {dir, off} {
 }
 
-Ray::direction() const {
+Vector Ray::direction() const {
 	return line.direction();
 }
 
-Ray::offset() const {
+Vector Ray::offset() const {
 	return line.offset();
 }

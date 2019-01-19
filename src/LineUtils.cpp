ee5cd0878586cd6aa105a1feb94333eab47fb5ae
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2019-01-18 22:02:10 -0500

    Added better line intersection implementation

--- src/LineUtils.cpp
+++ src/LineUtils.cpp
@@ -13,54 +13,43 @@ Vector normal(const Line& l, const Vector& v) {
 }
 
 Intersection intersection(const Line& l1, const Line& l2) {
-	AugmentedMatrix am {
-		{
-			{l1.direction().x(), -l2.direction().x()},
-				{l1.direction().y(), -l2.direction().y()},
-				{l1.direction().z(), -l2.direction().z()},
-		},
-			{
-				{-l1.offset().x() + l2.offset().x()},
-				{-l1.offset().y() + l2.offset().y()},
-				{-l1.offset().z() + l2.offset().z()},
-			}
-	};
-
-	const SolutionSet s {solution(rref_in_place(am))};
-	switch (s.first) {
-		case SolutionSetType::none:
-			return EmptySet {};
-		case SolutionSetType::unique:
-			assert(std::fabs(s.second[2][0]) <= ESP);
-			return evaluate(l1, s.second[0][0]);
-		case SolutionSetType::infinite:
+	// Test if the lines are really the same
+	if (l1.offset() != l2.offset()) {
+		const Vector nd {normalize(l1.direction())};
+		const Vector ndv {normalize(l2.offset() - l1.offset())};
+		if (ndv == nd || ndv == -nd)
 			return l1;
+	} else {
+		// if they have the same offset, we've found an intersection point, but still check
+		// whether the entire line intersections
+		const Vector nd1 {normalize(l1.direction())};
+		const Vector nd2 {normalize(l2.direction())};
+
+		return (nd1 == nd2 || nd1 == -nd2) ? Intersection {l1} : Intersection {l1.offset()};
 	}
+
+	const Vector e {l1.direction()};
+	const Vector f {l2.direction()};
+	const Vector g {l2.offset() - l1.offset()};
+	const Vector h {f % g};
+	const Vector k {f % e};
+	const double hm {magnitude(h)};
+	const double km {magnitude(k)};
+
+	if (std::fabs(hm) <= ESP || std::fabs(km) <= ESP)
+		return EmptySet {};
+	
+	const int s {(h*k) >= 0 ? 1 : -1};
+
+	return l1.offset() + e*s*(hm/km);
 }
 
 Intersection intersection(const Line& l, const Vector& v) {
-	AugmentedMatrix am {
-		{
-			{l.direction().x()},
-				{l.direction().y()},
-				{l.direction().z()},
-		},
-			{
-				{-l.offset().x() + v.x()},
-				{-l.offset().y() + v.y()},
-				{-l.offset().z() + v.z()},
-			}
-	};
+	if (l.offset() != v) {
+		const Vector nd1 {normalize(v - l.offset())};
+		const Vector nd2 {normalize(l.direction())};
 
-	const SolutionSet s {solution(rref_in_place(am))};
-	switch (s.first) {
-		case SolutionSetType::none:
-			return EmptySet {};
-		case SolutionSetType::unique:
-			assert(std::fabs(s.second[2][0]) <= ESP);
-			return v;
-		case SolutionSetType::infinite:
-			assert(false);
-			return EmptySet {};
-	}
+		return (nd1 == nd2 || nd1 == -nd2) ? Intersection {v} : Intersection {EmptySet {}};
+	} else
+		return v;
 }

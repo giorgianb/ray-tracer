fa655f3d4610a9cb828a5dd2d94e0e083d2badf2
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2019-01-18 22:35:52 -0500

    Updated for Polygon

--- generate.py
+++ generate.py
@@ -3,6 +3,12 @@ import sys
 from random import randrange
 import math
 
+def roundt(t):
+    l = []
+    for i in t:
+        l.append(round(i, 3))
+    return tuple(l)
+
 def main():
     colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
     with open(sys.argv[1], 'w') as f:
@@ -37,20 +43,43 @@ def main():
         NPOLY = 20
         for radius in range(1, 5):
             for i in range(NPOLY):
+#                break
                 theta = 2*math.pi*i/NPOLY
+
                 xoff = radius * 100 * math.cos(theta)
                 yoff = radius * 100 * math.sin(theta)
                 zoff = 500+(radius*(NPOLY + 1)) + i
+        
+                dx1 = 100*math.cos(theta)
+                dy1 = 100*math.sin(theta)
+                dx2 = 100*math.cos(theta + math.pi/2)
+                dy2 = 100*math.sin(theta + math.pi/2)
 
-                p1 = (100*math.cos(theta) + xoff, yoff, zoff)
-                p2 = (xoff, 100*math.sin(theta) + yoff, zoff)
+                p1 = (xoff + dx1, yoff + dy1, zoff)
+                p2 = (xoff + dx2, yoff + dy2, zoff)
                 p3 = (xoff, yoff, zoff)
                 color = colors[abs(i) % len(colors)]
-                write_convex_polygon(
-                        define_convex_polygon_from_points(color, p1, p2, p3, p1), 
+#                write_convex_polygon(
+#                        define_convex_polygon_from_points(color, p1, p2, p3, p1), 
+#                        file=f)
+                print(roundt(p1), roundt(p2), roundt(p3), roundt(p1))
+                write_polygon(
+                        define_polygon_from_points(color, p1, p2, p3, p1), 
                         file=f)
-            
-            
+
+#        radius = 0
+#        theta = 0
+#        xoff = radius * 100 * math.cos(theta)
+#        yoff = radius * 100 * math.sin(theta)
+#        zoff = 500+(radius*(NPOLY + 1)) + i
+#        p1 = (100*math.cos(theta) + xoff, yoff, zoff)
+#        p2 = (xoff, 100*math.sin(theta) + yoff, zoff)
+#        p3 = (xoff, yoff, zoff)
+#        color = colors[0]
+#        write_polygon(
+#                define_polygon_from_points(color, p1, p2, p3, p1), 
+#                file=f)
+
 
 def scale(k, v):
     return tuple(k*e for e in v)
@@ -92,6 +121,14 @@ def define_convex_polygon_from_points(color, *points):
 
     return ((base, tuple(edges)), color)
 
+def define_polygon_from_points(color, *points):
+    assert len(points) >= 4
+    assert points[0] == points[-1]
+
+    base = define_plane_from_points(color, *points[:3])[0]
+    return ((base, tuple(points)), color)
+
+
 def write_convex_polygon(convex_polygon, file=sys.stdout):
     print("ConvexPolygon", file=file)
     base, edges = convex_polygon[0]
@@ -101,6 +138,17 @@ def write_convex_polygon(convex_polygon, file=sys.stdout):
         print("{} {} {}\t{} {} {}".format(*edge), file=file)
 
     print("{} {} {}".format(*convex_polygon[1]), file=file)
+
+def write_polygon(polygon, file=sys.stdout):
+    print("Polygon", file=file)
+    base, vertices = polygon[0]
+    print("{} {} {}\t{} {} {}\t{} {} {}".format(*base), file=file)
+    print(len(vertices), file=file)
+    for vertex in vertices:
+        print("{} {} {}".format(*vertex), file=file)
+
+    print("{} {} {}".format(*polygon[1]), file=file)
+
 
 if __name__ == '__main__':
     main()

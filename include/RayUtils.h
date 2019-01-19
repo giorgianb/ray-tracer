65fbd04c5a6cc34c482426c9c899ef7a6320796e
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2019-01-18 23:56:20 -0500

    Made RayUtils constexpr

--- include/RayUtils.h
+++ include/RayUtils.h
@@ -2,10 +2,44 @@
 #define __RAY_TRACER_RAY_UTILS_INCLUDED
 #include "Ray.h"
 #include "Intersection.h"
+#include "LineUtils.h"
 
-MaybeVector evaluate(const Ray& r, const double& s);
-Vector normal(const Ray& r, const Vector& v);
-Intersection intersection(const Ray& r, const Line& l);
-Intersection intersection(const Ray& r, const Vector& v);
+constexpr MaybeVector evaluate(const Ray& r, const double& s) noexcept;
+constexpr Vector normal(const Ray& r, const Vector& v) noexcept;
+constexpr Intersection intersection(const Ray& r, const Line& l) noexcept;
+constexpr Intersection intersection(const Ray& r, const Vector& v) noexcept;
+
+constexpr MaybeVector evaluate(const Ray& r, const double& s) noexcept {
+	if (s >= 0)
+		return evaluate(Line {r.direction(), r.offset()}, s);
+
+	return {};
+}
+
+constexpr Vector normal(const Ray& r, const Vector& v) noexcept {
+	return normal(Line {r.direction(), r.offset()}, v);
+}
+
+constexpr Intersection intersection(const Ray& r, const Line& l) noexcept {
+	const Intersection inter {intersection(Line {r.direction(), r.offset()}, l)};
+
+	if (const auto solp = std::get_if<Vector>(&inter))
+		return ((*solp - r.offset()) * r.direction()) >= 0 ?
+			Intersection {*solp} : Intersection {EmptySet {}};
+	else if (std::holds_alternative<Line>(inter))
+		return r;
+	else
+		return EmptySet {};
+}
+
+constexpr Intersection intersection(const Ray& r, const Vector& v) noexcept {
+	const Intersection inter {intersection(Line {r.direction(), r.offset()}, v)};
+
+	if (const auto solp = std::get_if<Vector>(&inter))
+		return ((*solp - r.offset()) * r.direction()) >= 0 ?
+			Intersection {*solp} : Intersection {EmptySet {}};
+	else
+		return EmptySet {};
+}
 
 #endif

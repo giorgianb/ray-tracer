14a32f7f71af6978b087f56aa3a422684972fb98
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@protonmail.com>
2019-01-20 16:27:30 -0500

    Removed ConvexPolygon  from build

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-g++ -o rt -std=c++17 -O2 src/main.cpp src/Sphere.cpp src/Surface.cpp src/Tracer.cpp src/SurfacePlane.cpp src/Linear.cpp src/SurfaceConvexPolygon.cpp src/ConvexPolygon.cpp src/SurfacePolygon.cpp src/Polygon.cpp src/PolygonUtils.cpp src/Debug.cpp src/ConvexPolygonUtils.cpp -Iinclude/ 
+g++ -o rt -std=c++17 -O2 src/main.cpp src/Sphere.cpp src/Surface.cpp src/Tracer.cpp src/SurfacePlane.cpp src/Linear.cpp src/SurfacePolygon.cpp src/Polygon.cpp src/PolygonUtils.cpp src/Debug.cpp -Iinclude/ 
